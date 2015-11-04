21d5743824d58e3499a426e205013929f4332a0c
Tomoyuki KANO <tomo@appletz.jp>
2015-11-04 15:06:05 +0900

    update tickets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,4 @@ class ApplicationController < ActionController::Base
       redirect_to root_url
     end
   end
-
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,26 @@
 class TicketsController < ApplicationController
+  before_action authenticate_user!, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.user = current_user
+    if @ticket.save
+      redirect_to '/users'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:message)
+  end
 end
