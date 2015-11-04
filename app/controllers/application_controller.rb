a6f9947018619953ba91325405ade2ac42428aeb
Tomoyuki KANO <tomo@appletz.jp>
2015-11-04 09:11:24 +0900

    add admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def logged_in!
+    unless user_logged_in? || admin_loged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to root_url
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,20 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/show'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/session',
+    passwords: 'users/password',
+    registrations: 'users/registration'
+  }
+
+  resources :admins, only: [:show, :index]
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/session',
+    passwords: 'admins/password',
+    registrations: 'admins/registration'
+  }
+
+  resources :admins, only: [:show, :index]
+
 end
